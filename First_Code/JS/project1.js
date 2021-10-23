8fe4497bf31e5360df168f502c5b8fe7eb65c52b
Fasuto1st <nicha.tnct@gmail.com>
2021-10-23 15:16:58 +0700

    Update!

---
+++ First_Code/JS/project1.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "<h>Project 1</h>";
+    document.getElementById("demo1").innerHTML = "Hello Malikan!";
+    document.getElementById("demo2").innerHTML ="<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptas pariatur repellat veritatis atque, tempora quasi quas facere impedit aliquam libero qui iure cumque incidunt facilis soluta necessitatibus laboriosam nemo. Delectus architecto";
+    document.getElementById("demo3").innerHTML ="  sed, excepturi natus iste vel quidem officia corrupti repudiandae!</p>";
+    document.getElementById("demo4").innerHTML ="<p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, deleniti eaque eligendi minus maxime tempora eiusi.</p>";
+    document.getElementById("demo5").innerHTML ="<p>Have a nice day</p>";
+}
---
+++ First_Code/JS/project6.js
@@ -0,0 +1,8 @@
+function myFunction6() {
+    document.getElementById("demo").innerHTML = "<h>Project 6</h>";
+    document.getElementById("demo1").innerHTML = "Hello Malikan!22";
+    document.getElementById("demo2").innerHTML ="<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptas pariatur repellat veritatis atque, tempora quasi quas facere impedit aliquam libero qui iure cumque incidunt facilis soluta necessitatibus laboriosam nemo. Delectus architecto";
+    document.getElementById("demo3").innerHTML ="  sed, excepturi natus iste vel quidem officia corrupti repudiandae!</p>";
+    document.getElementById("demo4").innerHTML ="<p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, deleniti eaque eligendi minus maxime tempora eiusi.</p>";
+    document.getElementById("demo5").innerHTML ="<p>Have a nice day</p>";
+}
