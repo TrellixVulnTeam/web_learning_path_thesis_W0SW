ff989e28a79d4e3f534cef09db580a47bb3f722f
NEWSIX <nitigon.phad@gmail.com>
2021-10-29 19:40:15 +0700

    Project was changed.

--- First_Code/JS/project1.js
+++ First_Code/JS/project1.js
@@ -1,9 +1,16 @@
+
+
 function myFunction() {
-    document.getElementById("demo").innerHTML = "<h>Project 1</h>";
-    document.getElementById("demo1").innerHTML = "Hello Malikan!";
+    document.getElementById("demo").innerHTML = "<h>Project 1      ##### id = demo </h>";
+    document.getElementById("demo1").innerHTML = "Hello Malikan!      ##### id = demo1 ";
     document.getElementById("demo2").innerHTML ="<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptas pariatur repellat veritatis atque, tempora quasi quas facere impedit aliquam libero qui iure cumque incidunt facilis soluta necessitatibus laboriosam nemo. Delectus architecto</p>";
     document.getElementById("demo3").innerHTML =" <p> sed, excepturi natus iste vel quidem officia corrupti repudiandae!</p>";
     document.getElementById("demo4").innerHTML ="<p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, deleniti eaque eligendi minus maxime tempora eiusi.</p>";
     document.getElementById("demo5").innerHTML ="<p>Have a nice day</p>";
-    document.getElementById("demo6").innerHTML = "<div class='topnav-right'><a  class='button1' style='vertical-align:middle' ><span>Start</span></a></div>"
+
+    var s = "https://avatars.githubusercontent.com/u/60424171?v=4";
+    var bnt = "<p class='button1' style='vertical-align:middle' ><span> Start </span></p>";
+    var result = bnt.link(s);
+    document.getElementById("project-click").innerHTML = result;
+
 }
