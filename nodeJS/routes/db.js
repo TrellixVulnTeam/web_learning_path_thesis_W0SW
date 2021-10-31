7a1d92aaa44804e3b75d6a697a63bf9756985259
NEWSIX <nitigon.phad@gmail.com>
2021-11-01 00:59:56 +0700

    db

--- nodeJS/routes/db.js
+++ nodeJS/routes/db.js
@@ -1,29 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-//เรียก module mobogodb
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/thesisDB";
+var url = "mongodb://localhost:27017/";
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
     MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("thesisDB");  
-      var query = { a:12323};
-      dbo.collection("movie").find(query).toArray(function(err, result) {
-      //console.log("\n###############\n",result,"\n###############\n");
-      
-        var rt=result[1];
+    if (err) throw err;
+    var dbo = db.db("thesisDB");
+    dbo.collection("movie").findOne({}, function(err, result) {
         if (err) throw err;
-        //var rt = {"name":"India"};
+        console.log(result.name);
         db.close();
         res.render('db', { title: "我们 Database" 
-                            ,name: rt.name 
+                            ,name: result.name 
                         });  
-        });
     });
-  });
-  
-  
-  module.exports = router;
+    });
+});
+
+module.exports = router;
