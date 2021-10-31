77e3099a53792457c23ab3297a139ba5e753d5db
NEWSIX <nitigon.phad@gmail.com>
2021-11-01 02:20:14 +0700

    git

--- nodeJS/routes/db.js
+++ nodeJS/routes/db.js
@@ -3,21 +3,11 @@ var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-
-
 router.get('/', function(req, res, next) {
-    MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("thesisDB");
-    dbo.collection("movie").findOne({}, function(err, result) {
-        if (err) throw err;
-        console.log(result.name);
-        db.close();
-        res.render('db', { title: "我们 Database" 
-                            ,name: result.name 
-                        });  
-    });
-    });
-});
 
+    res.render('db', { title: "我们 Database" 
+    //                        ,name: result.name 
+    ,name: "CONNECTED" 
+                        });  
+});
 module.exports = router;
