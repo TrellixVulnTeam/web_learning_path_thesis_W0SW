0aa554a358523066bb9be5b5f6f9496a78fd7025
NEWSIX <nitigon.phad@gmail.com>
2021-11-03 18:18:16 +0700

    MMMMMMMMMMMMM

--- nodeJS/routes/quiz/TestMiniProject.js
+++ nodeJS/routes/quiz/TestMiniProject.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('test/TestMiniproject/main');
+  res.render('test/TestMiniproject/Main');
 });
 
 router.post('/',function(req,res){
