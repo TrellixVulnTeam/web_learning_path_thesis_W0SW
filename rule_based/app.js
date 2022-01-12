9ad3f39e611a93f17e81cd3f1406e6a933ffeee0
NEWSIX <nitigon.phad@gmail.com>
2022-01-12 16:36:22 +0700

    1-12-2022

--- rule_based/app.js
+++ rule_based/app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var databaseRouter = require('./routes/database');
+var calRouter = require('./routes/calculate');
 
 
 var app = express();
@@ -21,9 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/', databaseRouter);
+app.use('/cal', calRouter);
+
 
 
 // catch 404 and forward to error handler
